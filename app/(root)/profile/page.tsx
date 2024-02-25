b09bdea0303dcddbfce1e84c8088bf1f92e2489a
Eduardq <104719717+Eduardq@users.noreply.github.com>
2024-02-25 17:55:14 +0200

    finished

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,44 +1,56 @@
 import Collection from "@/components/shared/Collection";
 import { Button } from "@/components/ui/button";
 import { getEventsByUser } from "@/lib/actions/event.actions";
+import { getOrdersByUser } from "@/lib/actions/order.actions";
+import { IOrder } from "@/lib/database/models/order.model";
+import { SearchParamProps } from "@/types";
 import { auth } from "@clerk/nextjs";
 import Link from "next/link";
 import React from "react";
 
-const page = async () => {
+const ProfilePage = async ({ searchParams }: SearchParamProps) => {
   const { sessionClaims } = auth();
   const userId = sessionClaims?.userId as string;
 
-  const organizedEvents = await getEventsByUser({ userId, page: 1 });
+  const ordersPage = Number(searchParams?.ordersPage) || 1;
+  const eventsPage = Number(searchParams?.eventsPage) || 1;
+
+  const orders = await getOrdersByUser({ userId, page: ordersPage });
+
+  const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
+  const organizedEvents = await getEventsByUser({ userId, page: eventsPage });
+
   return (
     <>
+      {/* My Tickets */}
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <div className="wrapper flex items-center justify-center sm:justify-between">
           <h3 className="h3-bold text-center sm:text-left">My Tickets</h3>
           <Button asChild size="lg" className="button hidden sm:flex">
-            <Link href="/#events">Explore more events</Link>
+            <Link href="/#events">Explore More Events</Link>
           </Button>
         </div>
       </section>
 
       <section className="wrapper my-8">
         <Collection
-          data={organizedEvents?.data}
-          emptyTitle="No Event tickets purchased yet"
-          emptyStateSubtext="No worries plenty of exciting events to explore!"
+          data={orderedEvents}
+          emptyTitle="No event tickets purchased yet"
+          emptyStateSubtext="No worries - plenty of exciting events to explore!"
           collectionType="My_Tickets"
-          limit={6}
-          page={1}
+          limit={3}
+          page={ordersPage}
           urlParamName="ordersPage"
-          totalPages={2}
+          totalPages={orders?.totalPages}
         />
       </section>
 
+      {/* Events Organized */}
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <div className="wrapper flex items-center justify-center sm:justify-between">
           <h3 className="h3-bold text-center sm:text-left">Events Organized</h3>
           <Button asChild size="lg" className="button hidden sm:flex">
-            <Link href="/event/create">Create New Event</Link>
+            <Link href="/events/create">Create New Event</Link>
           </Button>
         </div>
       </section>
@@ -46,17 +58,17 @@ const page = async () => {
       <section className="wrapper my-8">
         <Collection
           data={organizedEvents?.data}
-          emptyTitle="No Events have been created yet"
-          emptyStateSubtext="Create some"
+          emptyTitle="No events have been created yet"
+          emptyStateSubtext="Go create some now"
           collectionType="Events_Organized"
-          limit={6}
-          page={1}
+          limit={3}
+          page={eventsPage}
           urlParamName="eventsPage"
-          totalPages={2}
+          totalPages={organizedEvents?.totalPages}
         />
       </section>
     </>
   );
 };
 
-export default page;
+export default ProfilePage;
